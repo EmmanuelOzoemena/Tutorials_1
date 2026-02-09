0ece21509ab956d86990ecbc433b63323e15f19b
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2026-02-09 19:52:58 +0100

    practice global codes

--- NodeJS Tutorial/test.js
+++ NodeJS Tutorial/test.js
@@ -1,3 +1,4 @@
+/*
 const name = "Ebuka";
 
 console.log(name);
@@ -8,3 +9,23 @@ const greet = (name) => {
 
 greet("Ebuka");
 greet("Ozo");
+*/
+
+// Global object
+// console.log(global)
+/*
+global.setTimeout(() => {
+  console.log("in the terminal");
+  clearInterval(interval)
+}, 3000);
+
+
+const interval = setInterval(() => {
+  console.log("In the interval")
+}, 1000)
+*/
+/*
+console.log(__dirname)
+console.log(__filename)
+*/
+
