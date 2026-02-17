5a6aa88fe7a9aca4a69acfb0da6e6fd56330f1d0
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2026-02-17 13:21:15 +0100

    used and practiced ejs as a view engine

--- NodeJS Tutorial/app.js
+++ NodeJS Tutorial/app.js
@@ -3,6 +3,9 @@ const express = require("express");
 // Express app
 const app = express();
 
+// Register view engine
+app.set("view engine", "ejs");
+
 // Listen for request
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -10,19 +13,36 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
   //   res.send("<h2>Hello World!</h2>");
-  res.sendFile("./views/index.html", { root: __dirname });
-});
-app.get("/about", (req, res) => {
-  //   res.send("<h2>About Page</h2>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  // res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeats browser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
-// Redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/about", (req, res) => {
+  //   res.send("<h2>About Page</h2>");
+  // res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", { root: __dirname })
-})
+  // res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
+});
--- NodeJS Tutorial/people.js
+++
@@ -1,9 +0,0 @@
-const people = ["Ebuka", "Ozo", "Chi", "Kaycee"];
-const age = [23, 3, 4, 54];
-
-console.log(people);
-
-module.exports = {
-  people,
-  age,
-};
